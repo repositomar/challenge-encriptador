980bf9cb731f83a4a0fcbd81905755e7b509a114
OmarHernandez <ing.omar.hernandezv@gmail.com>
2023-05-30 13:18:55 -0600

    Add validate text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inputText = document.querySelector('.input-text');
 const outputText = document.querySelector('.output-text');
 const buttonCopy = document.querySelector('.btn-copy')
-buttonCopy.style.display = "none"
+buttonCopy.style.display = 'none'
 const STRINGVALUES = {
   'e': 'enter',
   'i': 'imes',
@@ -11,18 +11,21 @@ const STRINGVALUES = {
 }
 
 function encodeText() {
-  const stringEncode = encode(inputText.value);
-  outputText.value = stringEncode;
-  outputText.style.backgroundImage = 'none';
-  inputText.value = '';
-  buttonCopy.style.display = 'block';
+  if(!validateText()) {
+    const stringEncode = encode(inputText.value).toLowerCase();
+    outputText.value = stringEncode;
+    outputText.style.backgroundImage = 'none';
+    inputText.value = '';
+    buttonCopy.style.display = 'block';
+  }
 }
 
 function decodeText() {
-  const stringDecode = decode(inputText.value);
+  const stringDecode = decode(inputText.value).toLowerCase();
   outputText.value = stringDecode;
   outputText.style.backgroundImage = 'none';
   inputText.value = '';
+  buttonCopy.style.display = 'block';
 }
 
 function encode(stringToEncode) {
@@ -47,4 +50,28 @@ function decode(stringToDecode) {
     }
   }
   return stringToDecode;
+}
+
+function copyText() {
+  outputText.select();
+  navigator.clipboard.writeText(outputText.value)
+  outputText.value = '';
+  alert('Texto Copiado')
+}
+
+function validateText() {
+  let text = document.querySelector('.input-text').value;
+  let validator = text.match(/^[a-z]*$/);
+
+  if (text === '') {
+    alert('Ingresa algún texto')
+    location.reload();
+    return true;
+  }
+
+  if(!validator || validator === 0) {
+    alert('Solo son permitidas letras minúsculas y sin acentos')
+    location.reload();
+    return true;
+  }
 }
